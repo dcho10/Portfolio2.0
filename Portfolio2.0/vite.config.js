5ac4cc465940bf97b5b02fae68f55506caeca707
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-05-25 23:00:50 -0400

    Adjusted paths to files

--- Portfolio2.0/vite.config.js
+++ Portfolio2.0/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html"
+    }
+  },
   server: {
     port: 3000,
     open: true,
